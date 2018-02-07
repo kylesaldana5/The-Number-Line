6547eaaa9b1f49265ec55199ab6d6a2baa527d95
kylesaldana5 <kylesaldana5@gmail.com>
2018-02-07 13:29:25 -0600

    have shows by year showing details and printing to the dom

--- app/controllers/setlistsByYearCtrl.js
+++ app/controllers/setlistsByYearCtrl.js
@@ -5,26 +5,30 @@ angular.module("theNumberLine").controller("setlistsByYearCtrl", function ($scop
 
     // grabs the route params and sets it to get shows for that year
     let yearId = $routeParams.yearValue;
-    
+
     // function tha calls factoy to display the shows for the year the user clicked on via Era
-        $scope.showYear = () => {
-            setlistFactory.getShowByYear(yearId)
-                .then((shows) =>{
-                    $scope.shows = shows.data.data;
-                });
-        };    
+    $scope.showYear = () => {
+        setlistFactory.getShowByYear(yearId)
+            .then((shows) => {
+                $scope.shows = shows.data.data;
+                console.log('shows', $scope.shows);
+
+            });
+    };
+
     // function that will get show detials for the paticular show that year
-        $scope.showsThisYear = (details) =>{
-            setlistFactory.getShowDataByDate(details)
-                .then((details) =>{
-                    $scope.songs = details.data.data.tracks;
-                    $scope.venue = details.data.data.venue;
-                    $scope.mustShowButton = true;
-                    $scope.showObject = {
-                        showId: details.data.data.id,
-                        userId: firebase.auth().currentUser.uid
-                    };
-                    
-                });
-        };
+    $scope.showsThisYear = (years) => {
+        setlistFactory.getShowDataByDate(years)
+            .then((shows) => {
+                console.log('shows', shows.data.data);
+                $scope.songs = shows.data.data.tracks;
+                $scope.venue = shows.data.data.venue;
+                // $scope.mustShowButton = true;
+                $scope.showObject = {
+                    showId: shows.data.data.id,
+                    userId: firebase.auth().currentUser.uid
+                };
+            });
+    };
 });
+
